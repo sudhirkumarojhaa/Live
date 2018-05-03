beb357b1723dbca3561e8ffabbf5a9335ee53090
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-03 09:54:03 +0530

    first commit for final design

---
+++ js/custom.js
@@ -0,0 +1,92 @@
+
+//splash screen js
+
+$(".splash-screen").show();
+
+setTimeout(function () {
+  $(".splash-screen").fadeOut();
+  $("body").show(300);
+}, 100);
+
+//Splash screen text js
+var aText = new Array(
+"The concept of Life and HTML is similar all you need is some styling to make it Awesome!!",
+);
+var iSpeed = 100; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter()
+{
+ sContents =  ' ';
+ iRow = Math.max(0, iIndex-iScrollAt);
+ var destination = document.getElementById("typedtext");
+
+ while ( iRow < iIndex ) {
+  sContents += aText[iRow++] + '<br />';
+ }
+ destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+ if ( iTextPos++ == iArrLength ) {
+  iTextPos = 0;
+  iIndex++;
+  if ( iIndex != aText.length ) {
+   iArrLength = aText[iIndex].length;
+   setTimeout("typewriter()", 500);
+  }
+ } else {
+  setTimeout("typewriter()", iSpeed);
+ }
+}
+typewriter();
+
+
+//banner js
+
+var aText = new Array(
+  "sudhir kumar",
+);
+var iSpeed = 500; // time delay of print out
+var iIndex = 0; // start printing array at this posision
+var iArrLength = aText[0].length; // the length of the text array
+var iScrollAt = 20; // start scrolling up at this many lines
+
+var iTextPos = 0; // initialise text position
+var sContents = ''; // initialise contents variable
+var iRow; // initialise current row
+
+function typewriter() {
+  sContents = ' ';
+  iRow = Math.max(0, iIndex - iScrollAt);
+  var destination = document.getElementById("logo");
+
+  while (iRow < iIndex) {
+    sContents += aText[iRow++] + '<br />';
+  }
+  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
+  if (iTextPos++ == iArrLength) {
+    iTextPos = 0;
+    iIndex++;
+    if (iIndex != aText.length) {
+      iArrLength = aText[iIndex].length;
+      setTimeout("typewriter()", 500);
+    }
+  } else {
+    setTimeout("typewriter()", iSpeed);
+  }
+}
+typewriter();
+
+
+$(document).scroll(function() {
+  var y = $(this).scrollTop();
+  if (y > 1200) {
+    $('.affix').show(200);
+  } else {
+    $('.affix').hide();
+  }
+});
