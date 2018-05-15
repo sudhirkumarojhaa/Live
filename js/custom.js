061dc5443635d22965e4d8eb2e36ca1b59d7a227
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-15 11:03:37 +0530

    added css and named images

--- js/custom.js
+++ js/custom.js
@@ -1,94 +1,33 @@
 
 //splash screen js
-
 $(".splash").show();
+$(".databox").hide();
 
 setTimeout(function () {
   $(".splash").fadeOut();
-  $("body").show(300);
-}, 1500);
-
-//Splash screen text js
-var aText = new Array(
-"The concept of Life and HTML is similar all you need is some styling to make it Awesome!!",
-);
-var iSpeed = 100; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
-
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
-
-function typewriter()
-{
- sContents =  ' ';
- iRow = Math.max(0, iIndex-iScrollAt);
- var destination = document.getElementById("typedtext");
-
- while ( iRow < iIndex ) {
-  sContents += aText[iRow++] + '<br />';
- }
- destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
- if ( iTextPos++ == iArrLength ) {
-  iTextPos = 0;
-  iIndex++;
-  if ( iIndex != aText.length ) {
-   iArrLength = aText[iIndex].length;
-   setTimeout("typewriter()", 500);
-  }
- } else {
-  setTimeout("typewriter()", iSpeed);
- }
-}
-typewriter();
+  $(".databox").show(500);
+}, 6000);
 
 
-//banner js
-
-var aText = new Array(
-  "sudhir kumar",
-);
-var iSpeed = 500; // time delay of print out
-var iIndex = 0; // start printing array at this posision
-var iArrLength = aText[0].length; // the length of the text array
-var iScrollAt = 20; // start scrolling up at this many lines
-
-var iTextPos = 0; // initialise text position
-var sContents = ''; // initialise contents variable
-var iRow; // initialise current row
-
-function typewriter() {
-  sContents = ' ';
-  iRow = Math.max(0, iIndex - iScrollAt);
-  var destination = document.getElementById("logo");
-
-  while (iRow < iIndex) {
-    sContents += aText[iRow++] + '<br />';
-  }
-  destination.innerHTML = sContents + aText[iIndex].substring(0, iTextPos) + "_";
-  if (iTextPos++ == iArrLength) {
-    iTextPos = 0;
-    iIndex++;
-    if (iIndex != aText.length) {
-      iArrLength = aText[iIndex].length;
-      setTimeout("typewriter()", 500);
-    }
-  } else {
-    setTimeout("typewriter()", iSpeed);
-  }
-}
-typewriter();
-
-
-$(document).scroll(function() {
-  var y = $(this).scrollTop();
-  if (y > 1200) {
-    $('.affix').show(200);
-  } else {
-    $('.affix').hide();
-  }
+$(document).ready(function () {
+    $("#div1").hide();
+    $("#div1").delay(6000).fadeIn(500);
 });
+window.onload = function () {
+  (function () {
+    var counter = 6;
+    setInterval(function () {
+      counter--;
+      if (counter >= 0) {
+        span = document.getElementById("count");
+        span.innerHTML = counter;
+      } else {
+        document.getElementById("count").style.display = "none";
+      }
+      if (counter === 0) {
+        clearInterval(counter);
+      }
+    }, 1000);
 
-
+  })();
+};
