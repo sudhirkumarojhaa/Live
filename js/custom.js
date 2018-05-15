536953fe4150edff0cb07a0b5f6fe1bde92d1e6b
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-15 11:13:14 +0530

    removed count down js

--- js/custom.js
+++ js/custom.js
@@ -9,25 +9,25 @@ setTimeout(function () {
 }, 6000);
 
 
-$(document).ready(function () {
-    $("#div1").hide();
-    $("#div1").delay(6000).fadeIn(500);
-});
-window.onload = function () {
-  (function () {
-    var counter = 6;
-    setInterval(function () {
-      counter--;
-      if (counter >= 0) {
-        span = document.getElementById("count");
-        span.innerHTML = counter;
-      } else {
-        document.getElementById("count").style.display = "none";
-      }
-      if (counter === 0) {
-        clearInterval(counter);
-      }
-    }, 1000);
+// $(document).ready(function () {
+//     $("#div1").hide();
+//     $("#div1").delay(6000).fadeIn(500);
+// });
+// window.onload = function () {
+//   (function () {
+//     var counter = 6;
+//     setInterval(function () {
+//       counter--;
+//       if (counter >= 0) {
+//         span = document.getElementById("count");
+//         span.innerHTML = counter;
+//       } else {
+//         document.getElementById("count").style.display = "none";
+//       }
+//       if (counter === 0) {
+//         clearInterval(counter);
+//       }
+//     }, 1000);
 
-  })();
-};
+//   })();
+// };
