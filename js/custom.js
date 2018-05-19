1b85b7bc6f3103581a5e459b989e7630b38e7fef
James Sudhir <jamessudhir@Jamess-Sudhir.local>
2018-05-19 16:53:16 +0530

    added new pages

--- js/custom.js
+++ js/custom.js
@@ -4,30 +4,7 @@ $(".splash").show();
 $(".databox").hide();
 
 setTimeout(function () {
-  $(".splash").fadeOut();
+  $(".splash").fadeOut(100);
   $(".databox").show(500);
-}, 6000);
+}, 4000);
 
-
-// $(document).ready(function () {
-//     $("#div1").hide();
-//     $("#div1").delay(6000).fadeIn(500);
-// });
-// window.onload = function () {
-//   (function () {
-//     var counter = 6;
-//     setInterval(function () {
-//       counter--;
-//       if (counter >= 0) {
-//         span = document.getElementById("count");
-//         span.innerHTML = counter;
-//       } else {
-//         document.getElementById("count").style.display = "none";
-//       }
-//       if (counter === 0) {
-//         clearInterval(counter);
-//       }
-//     }, 1000);
-
-//   })();
-// };
